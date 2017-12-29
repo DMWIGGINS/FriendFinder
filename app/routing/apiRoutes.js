2e58d8a21fe7aebe96aaa4bd84211874a3f79984
Doreen Wiggins <Wiggy77@aol.com>
2017-12-28 20:49:45 -0500

    links and buttons connected, logic for comparing friends scores added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,22 +4,41 @@
 var path = require("path");
 
 var friends = require("../data/friends");
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-// module.exports = function(app) {
-//   // HTML GET Requests
-//   // Below code handles when users "visit" a page.
-//   // In each of the below cases the user is shown an HTML page of content
-//   // ---------------------------------------------------------------------------
-//   app.get("/tables", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/tables.html"));
-//   });
-//   app.get("/reserve", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/reserve.html"));
-//   });
-//   // If no matching route is found default to home
-//   app.get("*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/reserve.html"));
-//   });
-// };
+
+module.exports = function (app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
+    app.post("/api/friends", function (req, res) {
+        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+        // It will do this by sending out the value "true" have a table
+        // req.body is available since we're using the body-parser middleware
+        var bestMatch = [];
+        for (x = 0; x < friends.length; x++) {
+            var total = 0;
+            for (i = 0; i < scores.length; i++) {
+                total = total + Math.abs(parseInt(newFriends.scores[i]) - parseInt(friends[x].scores[i]));
+            }
+            bestMatch.push(total);
+        }
+        var bestScore = Math.min(bestMatch);
+
+        pickMe = bestMatch.indexOf(bestScore);
+
+        res.json(friends[pickMe]);
+        console.log(res.json(friends[pickMe]));
+    });
+};
